80ce7d47419c812b1f53addb05cdde19cc98a7bc
Lyana <malaya95lyana@gmail.com>
2017-09-16 14:23:18 +0300

    added

--- ex3/print_ladder.c
+++ ex3/print_ladder.c
@@ -1,21 +1,21 @@
 #include <unistd.h>
 
-int main(void)
+int		main(void)
 {
-	int a;
-	int b;
+	int	a;
+	int	b;
 
-	a = 1;
-	while(a <= 5)
+	a = 5;
+	while(a >= 1)
 	{
 		b = 1;
 		while(b <= a)
 		{
-			write(1, "*", 1);
+			write(1, ".", 1);
 			b++;
 		}
 		write(1, "\n", 1);
-		a++;
+		a--;
 	}
 	return (0);
 }
